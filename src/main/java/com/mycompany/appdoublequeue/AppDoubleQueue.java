9bb9484fc55031236e7ee20aa6ba2556d3cb561d
xaviertoala <xaviertoala2006@gmail.com>
2025-11-11 16:52:44 -0500

    Gestion de tareas pendientes

---
+++ src/main/java/com/mycompany/appdoublequeue/AppDoubleQueue.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.appdoublequeue;
+
+public class AppDoubleQueue {
+
+    public static void main(String[] args) {
+        //Aplicacion de colas dobles en "Gestion de tareas pendientes": 
+        //Es una aplicacion de tareas, los usuarios pueden agregar tareas nuevas al inicio (mas urgentes) 
+        // o al final (menos urgentes). Tambien pueden eliminar tareas completadas desde cualquier extremo.
+        
+        
+        
+    }
+}
